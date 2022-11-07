b73b66274713f42aa1d180fb9035587edd230f41
Ian Morrill <ian@ianmorrill.com>
2022-11-07 17:46:13 -0500

    Initial Code

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import { IansPage } from "./pages/IansPage";
+
+// Add your import below!
+
+
+function Home({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      <Text style={{ textAlign: "center", fontSize: 40 }}>These people can use Git!</Text>
+
+      <Button
+        title="Ian"
+        onPress={() => navigation.navigate("IansPage")}
+      />
+
+      {/* Add yourself below! */}
+
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+
+        <Stack.Screen name="IansPage" component={IansPage} />
+
+        {/* Add yourself below! */}
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ pages/IansPage.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export function IansPage() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 40 }}>Ian Morrill</Text>
+        <Text>The treasurer of the club!</Text>
+      </View>
+    );
+  }
